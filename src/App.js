74b44354ee1a9fa87b14084630641752dc0de291
seung <seunghee9941@gmail.com>
2021-04-29 15:21:55 +0900

    init project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #101010;
+  justify-content: center;
+  align-items: center;
+`;
+
+const App = () => {
+    return (
+        <Container>
+        </Container>
+    );
+};
+
+export default App;
