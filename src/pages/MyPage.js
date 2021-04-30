e47a2facf673245e7d387203245b7f9889af2519
Yejin Lim <dla4777@gmail.com>
2021-04-30 23:57:02 +0900

    MyPage 구현

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,23 +1,161 @@
 import React from "react";
 import styled from 'styled-components/native';
+import {
+    FlatList,
+    Image,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
+import TopBar from "../components/TobBar";
+import {Album} from "../components/Album";
 
+//받아와야할 것, UserName, Image, introduce, 공개 허용한 곡의 정보, 좋아요 등록한 곡의 정보
+export const MyPage =() => {
+    return (
+    <Container>
+        <Header/>
+        <TopBar/>
+        <UserFirstView>
+            <StyledText>User Name</StyledText>
+            <UserSecondView>
+                <Image source={require('../../assets/images/dla.png')} style={{width:90, height:90, borderRadius: 12}}/>
+                <UserThirdView>
+                    <Text style={{color : '#ffffff', padding: 5}}>이제 작곡에 입문한 병아리 작곡가 입니다. {"\n"}피드백 환영합니다!</Text>
+                       <View style={{width:100}}>
+                           <ProfileButton title={"프로필편집"} color='#012345' style={{borderColor:'white', borderWidth:5}}/>
+                       </View>
+                </UserThirdView>
+            </UserSecondView>
+        </UserFirstView>
+
+        <OpenSingView>
+            <StyledText>공개 허용한 곡</StyledText>
+                <FlatList
+                    data={albums}
+                    keyExtractor={(item)=> item.id}
+                    showsHorizontalScrollIndicator={false}
+                    horizontal
+                    renderItem={({item}) => {
+                        return(
+                            <TouchableOpacity>
+                                <AlbumView>
+                                    <Album title={item.title} state={item.state} description={item.description}/>
+                                </AlbumView>
+                            </TouchableOpacity>
+                        );
+                    }}
+                />
+        </OpenSingView>
+        <LikeSingView>
+            <StyledText>스크랩한 곡</StyledText>
+            <FlatList
+                data={albums}
+                keyExtractor={(item)=> item.id}
+                showsHorizontalScrollIndicator={false}
+                horizontal
+                renderItem={({item}) => {
+                    return(
+                            <TouchableOpacity>
+                                <AlbumView>
+                                    <Album title={item.title} state={item.state} description={item.description}/>
+                                </AlbumView>
+                            </TouchableOpacity>
+                    );
+                }}
+            />
+
+        </LikeSingView>
+
+    </Container>
+    );
+};
+
+const albums = [
+    {
+        id: "1",
+        title: '조이',
+        state: 'LIKE_SMALL',
+        description: '깔깔깔',
+        time: '1:20',
+        artist:'보경',
+        liked: true
+    },
+    {
+        id: "2",
+        title: '슬픔이',
+        state: 'LIKE_SMALL',
+        description: '흐애애앵',
+        time: '1:20',
+        artist:'보경',
+        liked: true
+    },
+    {
+        id: "3",
+        title: '소심이',
+        state: 'LIKE_SMALL',
+        description: '슬퍼!',
+        time: '1:20',
+        artist:'보경',
+        liked: true
+    },
+];
+
+
+
+const Header = styled.View`
+    height: 20px;
+`;
 const Container = styled.View`
   flex:1;
   justify-content: center;
-  align-items: center;
   background-color: #101010;
 `;
-const ParentContainer = styled.View`
+const UserFirstView = styled.View`
   flex: 1;
+  padding: 20px 30px 10px 10px;
+  justify-content: center;
 `;
+const UserSecondView = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+const UserThirdView = styled.View`
+  padding: 10px;
+  justify-content: center;
+`;
+
+const OpenSingView = styled.View`
+  flex: 2;
+  padding: 10px;
+  justify-content: center;
+`;
+
+const LikeSingView = styled.View`
+  flex: 2;
+  padding: 10px;
+  justify-content: center;
+`;
+
+const AlbumView=styled.View`
+  margin-left: 4px;
+  margin-right: 4px;
+  flex:1;
+  justify-content: center;
+`;
+
 const StyledText=styled.Text`
-  font-size: 30px;
+  font-size: 20px;
   color: #ffffff;
+  padding-bottom: 3px;
 `;
-export const MyPage =() => {
-    return (
-        <Container>
-            <StyledText>MyPage</StyledText>
-        </Container>
-    );
-};
+
+const ProfileButton = styled.Button`
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 10px;
+  flex-direction: column;
+  border-color:white;
+  border-width: 2px;
+`;
