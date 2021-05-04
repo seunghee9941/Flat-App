5293e802de553071ffb82b1c73f86673cd7832db
seung <seunghee9941@gmail.com>
2021-05-04 18:29:47 +0900

    fix CommunityDetail page, Profile component

--- src/components/Input/CommentInput.js
+++ src/components/Input/CommentInput.js
@@ -4,25 +4,27 @@ import {Ionicons} from "@expo/vector-icons";
 import {Pressable} from "react-native";
 
 const Container = styled.SafeAreaView`
-    flex-direction: row;
+  
 `;
 
 const InputContainer = styled.View`
   border-radius: 5px;
-  width: 87%;
+  width: 95%;
   height: 40px;
   margin: 5px 5px 5px 10px;
   padding: 0 3% 0 3%;
   background-color: #2E2E2E;
   border-color: ${props => props.isFocus ? "#51CDDE":"#2E2E2E"};
   border-width: 1px;
+  flex-direction: row;
 `;
 
 const CommentInputText=styled.TextInput`
-  width: 100%;
+  width: 90%;
   height: 40px;
   font-size: 15px;
   color: #FFFFFF;
+  margin-right: 4%;
 `;
 
 const IconView = styled.View`
@@ -46,14 +48,14 @@ const CommentInput = () => {
                     onBlur={() => setCommentFocus(false)}
                     returnKeyType="done"
                 />
+                <IconView>
+                    <Pressable onPress={()=> null}>
+                        {({ pressed }) => (
+                            <Ionicons name="send-outline" size={25} color={pressed? "#3B848E" : "#51CDDE"} />
+                        )}
+                    </Pressable>
+                </IconView>
             </InputContainer>
-            <IconView>
-                <Pressable onPress={()=> null}>
-                    {({ pressed }) => (
-                        <Ionicons name="send-outline" size={30} color={pressed? "#3B848E" : "#51CDDE"} />
-                    )}
-                </Pressable>
-            </IconView>
         </Container>
     );
 };
