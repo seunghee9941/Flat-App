c239ae60bc6b7d7cc735e88a29d5c3f912eb2346
seung <seunghee9941@gmail.com>
2021-06-19 18:51:45 +0900

    fix logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,29 +1,31 @@
 import React from 'react';
+import { Text } from 'react-native';
 import styled from 'styled-components/native'
+import AppLoading from 'expo-app-loading';
+import { useFonts, Sriracha_400Regular } from '@expo-google-fonts/sriracha';
 
 const LogoView = styled.View`
   justify-content: center;
   align-self: stretch;
-  
   align-items: center;
-  background-color: #101010;
   padding: 0 10px;
   flex-direction: row;
 `;
 
-const LogoText= styled.Text`
-  font-size: 30px;
-  font-weight: bold;
-  color: white;
-`;
-
 const Logo = () => {
+    let [fontsLoaded] = useFonts({
+        Sriracha_400Regular,
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+
     return (
         <LogoView>
-            <LogoText>Flat</LogoText>
+            <Text style={{ color:'white', fontSize:30, fontFamily: 'Sriracha_400Regular'}}>flat</Text>
         </LogoView>
     );
 };
-
 
 export default Logo;
